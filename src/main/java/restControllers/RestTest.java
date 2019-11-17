5dd3ebc4416ef2c3cc520a0fd3882b368f68381a
ask <ask@kolczastekaktusy.pl>
2019-11-17 18:35:51 +0100

    initialWithConfig

---
+++ src/main/java/restControllers/RestTest.java
@@ -0,0 +1,16 @@
+package restControllers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+public class RestTest {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Hi Test!";
+    }
+}
